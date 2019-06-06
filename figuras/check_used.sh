1ae83aca3a9dd027a93611763442465d2fb99324
andresgomezvidal <andresgomezvidal@gmail.com>
2019-06-06 18:38:57 +0200

    minor corrections: check_used was not excluding the .pdf generated by the .eps glossary was not complete minor clarifications for the first increment

--- figuras/check_used.sh
+++ figuras/check_used.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-for i in *; do if [[ ! "$i" =~ ".sh" && -z "$(grep $i -R ../capitulos/*.tex)" ]]; then echo "$i"; fi  ; done
+for i in *; do if [[ ! "$i" =~ ".pdf" && ! "$i" =~ ".sh" && -z "$(grep $i -R ../capitulos/*.tex)" ]]; then echo "$i"; fi  ; done
